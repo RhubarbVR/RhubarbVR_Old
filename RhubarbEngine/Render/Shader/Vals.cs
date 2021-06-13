6d580aff5b5ff24b48c5edd7379ce80ea083bdab
Faolan <46481567+Faolan-Rad@users.noreply.github.com>
2021-06-13 19:31:20 -0400

    Added some more vals for shaders

--- RhubarbEngine/Render/Shader/Vals.cs
+++ RhubarbEngine/Render/Shader/Vals.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Numerics;
 using g3;
 
 namespace RhubarbEngine.Render.Shader.Vals
@@ -104,6 +105,102 @@ namespace RhubarbEngine.Render.Shader.Vals
             value = _value;
         }
     }
+    public struct Val_uvec2
+    {
+        Vector2u value;
+        public Val_uvec2(Vector2u _value)
+        {
+            value = _value;
+        }
+    }
+
+    public struct Val_uvec3
+    {
+        Vector3u value;
+        public Val_uvec3(Vector3u _value)
+        {
+            value = _value;
+        }
+    }
+
+    public struct Val_uvec4
+    {
+        Vector4u value;
+        public Val_uvec4(Vector4u _value)
+        {
+            value = _value;
+        }
+    }
+
+    public struct Val_vec2
+    {
+        Vector2f value;
+        public Val_vec2(Vector2f _value)
+        {
+            value = _value;
+        }
+    }
+
+    public struct Val_vec3
+    {
+        Vector3f value;
+        public Val_vec3(Vector3f _value)
+        {
+            value = _value;
+        }
+    }
+
+    public struct Val_vec4
+    {
+        Vector4f value;
+        public Val_vec4(Vector4f _value)
+        {
+            value = _value;
+        }
+    }
+
+    public struct Val_dvec2
+    {
+        Vector2d value;
+        public Val_dvec2(Vector2d _value)
+        {
+            value = _value;
+        }
+    }
+
+    public struct Val_dvec3
+    {
+        Vector3d value;
+        public Val_dvec3(Vector3d _value)
+        {
+            value = _value;
+        }
+    }
+    public struct Val_dvec4
+    {
+        Vector4d value;
+        public Val_dvec4(Vector4d _value)
+        {
+            value = _value;
+        }
+    }
+
+    public struct Val_mat2x2
+    {
+        Matrix2f value;
+        public Val_mat2x2(Matrix2f _value)
+        {
+            value = _value;
+        }
+    }
 
 
+    public struct Val_mat3x2
+    {
+        Matrix3x2 value;
+        public Val_mat3x2(Matrix3x2 _value)
+        {
+            value = _value;
+        }
+    }
 }
