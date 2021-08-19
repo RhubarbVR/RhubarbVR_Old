288c4023615148d3338ed837118661787f84f6ef
Faolan <46481567+Faolan-Rad@users.noreply.github.com>
2021-08-18 23:33:47 -0400

    Rhubarb Launcher

---
+++ RhubarbVRLauncher/Main.Designer.cs
@@ -0,0 +1,240 @@
+﻿
+namespace RhubarbVRLauncher
+{
+    partial class Main
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.StartButton = new System.Windows.Forms.Button();
+            this.Tittel = new System.Windows.Forms.Label();
+            this.githublink = new System.Windows.Forms.LinkLabel();
+            this.OutPutType = new System.Windows.Forms.ComboBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(250, 455);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(274, 80);
+            this.StartButton.TabIndex = 1;
+            this.StartButton.Text = "Start";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Tittel
+            // 
+            this.Tittel.AutoSize = true;
+            this.Tittel.Font = new System.Drawing.Font("Arial", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Tittel.Location = new System.Drawing.Point(57, 19);
+            this.Tittel.Name = "Tittel";
+            this.Tittel.Size = new System.Drawing.Size(660, 43);
+            this.Tittel.TabIndex = 2;
+            this.Tittel.Text = "Welcome To RhubarbVR PrePreAlpha";
+            this.Tittel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.Tittel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // githublink
+            // 
+            this.githublink.AutoSize = true;
+            this.githublink.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.githublink.Location = new System.Drawing.Point(625, 62);
+            this.githublink.Name = "githublink";
+            this.githublink.Size = new System.Drawing.Size(92, 21);
+            this.githublink.TabIndex = 3;
+            this.githublink.TabStop = true;
+            this.githublink.Text = "GitHub Link";
+            this.githublink.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // OutPutType
+            // 
+            this.OutPutType.AllowDrop = true;
+            this.OutPutType.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.OutPutType.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.OutPutType.DisplayMember = "Auto";
+            this.OutPutType.FormattingEnabled = true;
+            this.OutPutType.Items.AddRange(new object[] {
+            "Auto",
+            "Screen",
+            "SteamVR"});
+            this.OutPutType.Location = new System.Drawing.Point(250, 390);
+            this.OutPutType.Name = "OutPutType";
+            this.OutPutType.Size = new System.Drawing.Size(274, 23);
+            this.OutPutType.TabIndex = 4;
+            this.OutPutType.Tag = "";
+            this.OutPutType.Text = "Auto";
+            this.OutPutType.ValueMember = "Auto";
+            this.OutPutType.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(250, 419);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(119, 19);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "Clear Local World";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.checkBox2.Location = new System.Drawing.Point(757, 543);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(15, 14);
+            this.checkBox2.TabIndex = 6;
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.checkBox3.Location = new System.Drawing.Point(-1, 3);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(15, 14);
+            this.checkBox3.TabIndex = 7;
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.checkBox4.Location = new System.Drawing.Point(-1, 543);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(15, 14);
+            this.checkBox4.TabIndex = 8;
+            this.checkBox4.UseVisualStyleBackColor = true;
+            this.checkBox4.CheckedChanged += new System.EventHandler(this.checkBox4_CheckedChanged);
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.checkBox5.Location = new System.Drawing.Point(757, 3);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(15, 14);
+            this.checkBox5.TabIndex = 9;
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(250, 372);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 15);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Output Devices";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::RhubarbVRLauncher.Properties.Resources.RhubarbVR2;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(245, 78);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(284, 232);
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click_1);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(250, 346);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(274, 23);
+            this.textBox1.TabIndex = 12;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(250, 326);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 15);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Args";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(172)))), ((int)(((byte)(236)))));
+            this.ClientSize = new System.Drawing.Size(775, 557);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox5);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.OutPutType);
+            this.Controls.Add(this.githublink);
+            this.Controls.Add(this.Tittel);
+            this.Controls.Add(this.StartButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Main";
+            this.Text = "RhubarbVR Launcher";
+            this.Load += new System.EventHandler(this.Main_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Label Tittel;
+        private System.Windows.Forms.LinkLabel githublink;
+        private System.Windows.Forms.ComboBox OutPutType;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
