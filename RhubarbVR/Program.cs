1ee5b8d3e768e8c253dd14f5f543b99212bf4a0e
Faolan <46481567+Faolan-Rad@users.noreply.github.com>
2021-09-23 16:18:23 -0400

    Update Program.cs

--- RhubarbVR/Program.cs
+++ RhubarbVR/Program.cs
@@ -6,7 +6,7 @@ namespace RhubarbVR
 {
     public static class Program
     {
-        public static IEngine engine = new();
+        public static Engine engine = new();
         public static void Main(string[] _args)
         {
             try
