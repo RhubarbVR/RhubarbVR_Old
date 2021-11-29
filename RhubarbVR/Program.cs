ef9f228586534bfd143280380069070eddd67e81
Faolan <46481567+Faolan-Rad@users.noreply.github.com>
2021-11-28 20:54:05 -0500

    better crash management

--- RhubarbVR/Program.cs
+++ RhubarbVR/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.IO;
 using RhubarbEngine;
 
 namespace RhubarbVR
@@ -11,23 +11,38 @@ namespace RhubarbVR
         {
             try
             {
-                engine.Initialize<BaseEngineInitializer,UnitLogs>(_args);
-                engine.StartUpdateLoop();
+                var tempFile = AppDomain.CurrentDomain.BaseDirectory + Guid.NewGuid().ToString() + ".tmp";
+                using (File.Create(tempFile))
+                { }
+                File.Delete(tempFile);
+                try
+                {
+                    engine.Initialize<BaseEngineInitializer, UnitLogs>(_args);
+                    engine.StartUpdateLoop();
+                }
+                catch (Exception e)
+                {
+                    engine.Logger.Log(e.ToString(), true);
+                }
+                engine.Logger.Log("Closing Game", true);
+                try
+                {
+                    engine.CleanUP();
+                }
+                catch (Exception e)
+                {
+                    engine.Logger.Log("Failed To Close Game" + e.ToString(), true);
+                    engine.Logger.CleanUP();
+                }
             }
-            catch (Exception e)
+            catch(Exception e)
             {
-                engine.Logger.Log(e.ToString(), true);
+                while(Console.ReadKey().Key != ConsoleKey.Escape)
+                {
+                    Console.WriteLine("An Error Was encountered Click Esc to close Error:" + e.ToString());
+                }
             }
-            engine.Logger.Log("Closing Game", true);
-            try
-            {
-                engine.CleanUP();
-            }
-            catch (Exception e)
-            {
-                engine.Logger.Log("Failed To Close Game"+e.ToString(), true);
-                engine.Logger.CleanUP();
-            }
+
         }
     }
 }
